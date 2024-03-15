8ca964134a7032fcbe6cfb14a01dea9aa9eceffc
Guillaume BELDILMI <gbeldilmi@pm.me>
2024-03-15 11:12:41 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -28,25 +28,12 @@ fn hamming7(c1: u8, c2: u8, c3: u8, c4: u8, c5: u8, c6: u8, c7: u8) -> usize {
   let p1 = c1 ^ c2 ^ c3;
   let p2 = c1 ^ c2 ^ c4;
   let p3 = c2 ^ c3 ^ c4;
-  let e1 = c5 != p1;
-  let e2 = c6 != p2;
-  let e3 = c7 != p3;
-  match (e1, e2, e3) {
-    (true, true, false) => {
-      1
-    },
-    (true, true, true) => {
-      2
-    },
-    (true, false, true) => {
-      3
-    },
-    (false, true, true) => {
-      4
-    },
-    _ => {
-      0
-    }
+  match (c5 != p1, c6 != p2, c7 != p3) {
+    (true,  true,  false) => { 1 },
+    (true,  true,  true)  => { 2 },
+    (true,  false, true)  => { 3 },
+    (false, true,  true)  => { 4 },
+    _                     => { 0 }
   }
 }
 
@@ -59,11 +46,12 @@ fn correct_errors(input: Vec<u8>) -> Vec<u8> {
     }
     let c = hamming7(input[i], input[i+1], input[i+2], input[i+3], input[i+4], input[i+5], input[i+6]);
     for j in 0..7 {
-      if j + 1 != c {
-        corrected.push(input[i+j]);
-      } else {
-        corrected.push(if input[i+j] == 0 { 1 } else { 0 });
-      }
+      corrected.push(
+        if j + 1 != c {
+          input[i+j]
+        } else {
+          input[i+j] ^ 0x01
+        });
     }
     i += 7;
   }
@@ -117,11 +105,11 @@ fn decrypt(input: String) -> String {
 fn main() {
   let input = read_file("doc/lettre.txt");
   let corrected = correct_errors(input);
-  write_bin_file("target/1-corrected.txt", &corrected);
+  write_bin_file("target/--1-corrected.txt", &corrected);
   let reduced = reduce(corrected);
-  write_bin_file("target/2-reduced.txt", &reduced);
+  write_bin_file("target/--2-reduced.txt", &reduced);
   let ascii = convert_to_ascii(reduced);
-  write_text_file("target/3-ascii.txt", &ascii);
+  write_text_file("target/--3-ascii.txt", &ascii);
   let decrypted = decrypt(ascii);
-  write_text_file("target/4-decrypted.txt", &decrypted);
+  write_text_file("target/--4-decrypted.txt", &decrypted);
 }
